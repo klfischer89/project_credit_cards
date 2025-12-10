ad761809aa64fb4ce20dbdc0ff63fcda7eac0469
klfischer89 <fischer-k4@web.de>
2025-12-10 16:06:40 +0100

    generating heatmap of correlation

--- project_credit_cards.py
+++ project_credit_cards.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 df = pd.read_csv("./data/creditcard.csv.bz2")
+
+sns.heatmap(df.corr())
+plt.show()
